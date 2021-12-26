560d5211ed56b3bbc35d47c6e68cdd5954995172
pengyidong <1124666946@qq.com>
2021-12-26 18:48:11 +0800

    feat

---
+++ 前端面试题/手写js/promise.js
@@ -0,0 +1,8 @@
+const preloadImage = function (path) {
+    return new Promise(function (resolve, reject) {
+      const image = new Image();
+      image.onload  = resolve;
+      image.onerror = reject;
+      image.src = path;
+    });
+  };
